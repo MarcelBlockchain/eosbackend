fa14cea1cfef174019b52a58a4daed3c46a1cc65
Murray Rothbart <marcel@blockspace.ch>
2018-10-04 16:31:36 +0200

    added JWT support

--- api/controllers/account.js
+++ api/controllers/account.js
@@ -4,72 +4,53 @@ const jwt = require('jsonwebtoken')
 
 const Account = require('../models/account')
 
-// TODO: accounts needed at all, or solely jwt_deviceID sufficient?
-// creates account with deviceID & email and returns token to sign in
+// creates account with hashed deviceID & mongodb generated ID and returns token to sign in
 
-// FIXME: create acc using hashed device ID
-exports.get_token = (req, res, next) => {
-  const token = jwt.sign(
-    { deviceID: acc[0].deviceID },
-    process.env.JWT_KEY,
-    { expiresIn: '7d' } // TODO: when?
-  )
-
-  return res.status(200).json({ message: 'Auth successful', token })
-
-//   Account.find({ email: req.body.email }).exec().then(acc => {
-//     if (acc.length > 1) {
-//       return res.status(409).json({ message: 'Mail exists' })
-//     } else {
-//       bcrypt.hash(req.body.deviceID, 10, (err, hash) => {
-//         if (err) return res.status(500).json({ err })
-//         else {
-//           const account = new Account({
-//             _id: new mongoose.Types.ObjectId(),
-//             email: req.body.email,
-//             deviceID: hash
-//           })
-//           account.save().then(result => {
-//             console.log(result)
-//             res.status(201).json({ message: 'Account created' })
-//           })
-//             .catch(err => {
-//               console.log(err)
-//               res.status(500).json({ err })
-//             })
-//         }
-//       })
-//     }
-//   })
+exports.account_signup = (req, res, next) => {
+  // encrypt device ID, then search
+  bcrypt.hash(req.body.deviceID, 10, (err, hash) => {
+    if (err) return res.status(500).json({ err })
+    Account.find({ deviceID: hash }).exec().then(acc => {
+      if (acc.length >= 1) {
+        return res.status(409).json({ message: 'Account already exists' })
+      } else {
+        const account = new Account({
+          _id: new mongoose.Types.ObjectId(),
+          deviceID: hash
+        })
+        account.save().then(result => {
+          console.log(result)
+          res.status(201).json({ message: 'Account created' })
+        })
+          .catch(err => {
+            console.log(err)
+            res.status(500).json({ err })
+          })
+      }
+    })
+  })
 }
 
 exports.account_login = (req, res, next) => {
-  Account.find({ deviceID: req.body.deviceID }).exec().then(acc => {
-    if (acc.length < 1) {
-      return res.status(401).json({
-        message: 'Auth failed'
-      })
-    }
-    bcrypt.compare(req.body.deviceID, acc[0].deviceID, (err, result) => {
-      if (err) {
+  bcrypt.hash(req.body.deviceID, 10, (err, hash) => {
+    if (err || !hash) return res.status(500).json({ err })
+
+    Account.find({ deviceID: hash }).exec().then(acc => {
+      if (acc.length < 1) {
         return res.status(401).json({
           message: 'Auth failed'
         })
       }
-      if (result) {
-        // TODO: what requirements for sign in? Device ID, email ?
-        const token = jwt.sign(
-          { deviceID: acc[0].deviceID },
-          process.env.JWT_KEY,
-          { expiresIn: '7d' } // TODO: when?
-        )
-        return res.status(200).json({ message: 'Auth successful', token })
-      }
-      res.status(401).json({ message: 'Auth failed ' })
+      const token = jwt.sign(
+        { deviceID: acc[0].deviceID },
+        process.env.JWT_KEY,
+        { expiresIn: '7d' }
+      )
+      return res.status(200).json({ message: 'Auth successful', token })
     })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json({ err })
+      })
   })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({ err })
-    })
 }
